29dece882abdf797339b9949436c4086e5dd734a
malivartti <malivartti@yandex.ru>
2024-11-20 23:38:59 +0300

    add content to about us page

--- src/App/pages/AboutUsPage/AboutUsPage.tsx
+++ src/App/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,7 +1,21 @@
+import Text from "@/components/Text";
+import cls from './AboutUsPage.module.scss'
+
 const AboutUsPage = () => {
   return (
-    <div>
-      About Us
+    <div className={cls.AboutUsPage}>
+      <Text tag="p" view="p-20" className={cls.AboutUsPage__text}>
+        Welcome to Lalasia, your trusted destination for stylish and comfortable apparel.
+        Our mission is to provide high-quality, modern clothing that fits seamlessly into your everyday life.
+      </Text>
+      <Text tag="p" view="p-20" className={cls.AboutUsPage__text}>
+        At Lalasia, we prioritize customer satisfaction and carefully curate our products to meet your needs.
+         Whether you’re looking for the latest trends or timeless essentials, we’ve got you covered.
+      </Text>
+      <Text tag="p" view="p-20" className={cls.AboutUsPage__text}>
+        Join us on this journey as we redefine convenience and style in the world of online shopping.
+         Explore, shop, and experience the best with Lalasia!
+      </Text>
     </div>
   );
 };
