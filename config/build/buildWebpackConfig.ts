743de28226a846b3c87e66eabf5b641c4b79ef3f
malivartti <malivartti@yandex.ru>
2024-11-29 01:52:33 +0300

    back

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -15,7 +15,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     output: {
       filename: '[name].[contenthash].js',
       path: paths.build,
-      publicPath: isDev ? '/' : '/kts-frontend-project',
+      publicPath: '/',
       clean: true,
     },
     module: {
